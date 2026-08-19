f6afef3893fe507017ec9fd4fb79c636db19146b
LEE YONGHA <71704562+lyh030725@users.noreply.github.com>
2026-08-19 18:01:39 +0900

    add package init

---
+++ src/longspeech_eval/__init__.py
@@ -0,0 +1,5 @@
+"""MiniCPM-o 4.5 x Marco-LongSpeech evaluation utilities."""
+
+from .constants import DATASET_ID, MODEL_ID, TASKS, TEST_COUNTS
+
+__all__ = ["DATASET_ID", "MODEL_ID", "TASKS", "TEST_COUNTS"]
